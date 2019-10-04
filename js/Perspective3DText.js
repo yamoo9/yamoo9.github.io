5dc75e0f3387024c446fa374cc777094ca35226c
yamoo9 <yamoo9@naver.com>
2019-10-04 21:45:27 +0900

    모듈 찾기, 일정 확인 링크 수정

--- js/Perspective3DText.js
+++ js/Perspective3DText.js
@@ -1,6 +1,6 @@
 ;(function(global) {
 	'use strict'
-	
+
 	var mouse = {
 		_x: 0,
 		_y: 0,
@@ -18,10 +18,11 @@
 			return '(' + this.x + ', ' + this.y + ')'
 		},
 	}
-	
+
 	// class
-  var Perspective3DText = function(container, innerSelector) {
+	var Perspective3DText = function(container, innerSelector, perspective) {
 		this._container = container
+		this._perspective = perspective || 15
 		this._innerSelector = innerSelector || 'span'
 		this._init()
 	}
@@ -35,11 +36,17 @@
 
 	// instance members
 	Perspective3DText.prototype._init = function() {
-		this._container.innerHTML = 
-			'<'+ this._innerSelector +' class="Perspective3DText_inner">' + 
-				this._container.innerHTML + 
-			'</'+ this._innerSelector +'>'
-		this._inner = this._container.querySelector('.Perspective3DText_inner')
+		var container = this._container
+		container.style.perspective = this._perspective + 'px'
+		container.innerHTML =
+			'<' +
+			this._innerSelector +
+			' class="Perspective3DText_inner">' +
+			container.innerHTML +
+			'</' +
+			this._innerSelector +
+			'>'
+		this._inner = container.querySelector('.Perspective3DText_inner')
 		mouse.setOrigin(this._container)
 		this._bind()
 	}
@@ -50,7 +57,7 @@
 		container.onmouseleave = this._onMouseLeaveHandler.bind(this)
 		container.onmousemove = this._onMouseMoveHandler.bind(this)
 	}
-	Perspective3DText.prototype._onMouseEnterHandler = function(e) {		
+	Perspective3DText.prototype._onMouseEnterHandler = function(e) {
 		this._update(e)
 	}
 	Perspective3DText.prototype._onMouseLeaveHandler = function() {
@@ -64,20 +71,16 @@
 	Perspective3DText.prototype._update = function(e) {
 		mouse.updatePosition(e)
 		this._updateTransformStyle(
-			(mouse.y / this._inner.offsetHeight / 2).toFixed(2), 
+			(mouse.y / this._inner.offsetHeight / 2).toFixed(2),
 			(mouse.x / this._inner.offsetWidth / 2).toFixed(2)
 		)
 	}
 	Perspective3DText.prototype._updateTransformStyle = function(x, y) {
 		var inner = this._inner
 
-		inner.style.transform = 
-		inner.style.webkitTransform = 
-		inner.style.mozTransform = 
-		inner.style.msTransform = 
-		inner.style.oTransform = 'rotateX(' + x + 'deg) rotateY(' + y + 'deg)'
+		inner.style.transform = inner.style.webkitTransform = inner.style.mozTransform = inner.style.msTransform = inner.style.oTransform =
+			'rotateX(' + x + 'deg) rotateY(' + y + 'deg)'
 	}
-	
-	global.Perspective3DText = Perspective3DText
 
+	global.Perspective3DText = Perspective3DText
 })(window)
